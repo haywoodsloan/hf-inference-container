c5f8408007b7bcfb15de4bf0d887dfcda32ed1d2
Sloan Haywood <haywood.sloan@yahoo.com>
2025-05-14 19:06:06 -0400

    get inference working on a function

--- function_app.py
+++ function_app.py
@@ -9,7 +9,7 @@ import os
 task_type = os.environ.get("TASK_TYPE")
 model_name = os.environ.get("MODEL_NAME")
 
-model = pipeline(task=task_type, model=model_name, use_fast=True)
+inference = pipeline(task=task_type, model=model_name, use_fast=True)
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 
@@ -20,13 +20,14 @@ def invoke(req: func.HttpRequest) -> func.HttpResponse:
     )
 
     try:
-        input = model(req.get_json())
+        input = req.get_json()
         logging.info("Using JSON input for model")
     except:
         input = base64.b64encode(req.get_body()).decode("ascii")
         logging.info("Using binary input as a base64 string for model")
 
-    return func.HttpResponse(
-        json.dumps(model(input)),
-        status_code=200,
-    )
+    try:
+        output = inference(input)
+        return func.HttpResponse(json.dumps(output), status_code=200)
+    except Exception as e:
+        return func.HttpResponse(f"{e}", status_code=500)
