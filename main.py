797d43d14f1f1fc979b9fd0cda1300156350cb73
Sloan Haywood <haywood.sloan@yahoo.com>
2025-06-23 12:18:23 -0400

    enable opentelemetry for service

--- main.py
+++ main.py
@@ -3,39 +3,34 @@ import logging
 import os
 
 from transformers import pipeline
+from azure.monitor.opentelemetry import configure_azure_monitor
 from fastapi import FastAPI, Response, Request, status, Body, HTTPException
 from fastapi.responses import JSONResponse, PlainTextResponse
 from fastapi.exception_handlers import http_exception_handler
 
 task_type = os.environ.get("TASK_TYPE")
 model_name = os.environ.get("MODEL_NAME")
-api_key = os.environ.get("API_KEY")
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger("app")
 
-app = FastAPI()
+# Setup logging to app insights
+try:
+    configure_azure_monitor(logger_name="app")
+except Exception as e:
+    log.warning(f"Failed to configure azure monitoring: {e}")
 
+# Preload the model
 try:
     log.info(f"Loading inference model, task: {task_type}, model: {model_name}")
     inference = pipeline(task=task_type, model=model_name, device_map="auto")
     log.info(f"Model loaded successfully")
 except Exception as e:
-    log.info(f"Loading model failed: ${e}")
+    log.error(f"Loading model failed: ${e}")
     raise e
 
-
-@app.middleware("http")
-async def check_auth(request: Request, call_next):
-    if "Authorization" not in request.headers:
-        exc = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-        return await http_exception_handler(request, exc)
-
-    if request.headers["Authorization"] != f"Bearer {api_key}":
-        exc = HTTPException(status_code=status.HTTP_403_FORBIDDEN)
-        return await http_exception_handler(request, exc)
-
-    return await call_next(request)
+# Initialize the server
+app = FastAPI()
 
 
 @app.options("/invoke")
@@ -58,5 +53,5 @@ def invoke_post(body=Body()):
         output = inference(input)
         return JSONResponse(output, status_code=200)
     except Exception as e:
-        log.info(f"Inference failed: ${e}")
+        log.warning(f"Inference failed: ${e}")
         return PlainTextResponse(f"[INFERENCE FAILED]: {e}", status_code=500)
